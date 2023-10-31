965281e4d279f6c9e768c7bd45f7605ddad6810c
Camie <camie@MacBook-Pro-648.lan>
2023-10-30 23:24:31 -0700

    Adding MongoDB

---
+++ packages/express-backend/Models/toDoList-services.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import toDoModel from "./toDoList";
+
+mongoose.set("debug", true);
+
+mongoose
+    .connect("mongodb://localhost:27017/myToDo", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((error) => console.log(error));
+
+function sortToDoByDate(date) {
+    return toDoModel.find().sort({ due: 1 });
+}
+
+function findToDoByDate(date) {
+    return toDoModel.find({ due: date });
+}
+
+function addTask(task) {
+    const taskToAdd = new toDoModel(task);
+    const promise = taskToAdd.save();
+    return promise;
+}
+
+export default {
+    sortToDoByDate,
+    findToDoByDate,
+    addTask,
+};
