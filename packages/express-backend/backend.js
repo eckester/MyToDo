89480e43de01c2548230491cdb87049dde3c3785
bellaboulais <bjboulai@calpoly.edu>
2023-11-03 14:07:08 -0700

    backend db

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,145 +1,70 @@
+// backend.js
 import express from "express";
 import cors from "cors";
- 
+import toDoListServices from "./models/toDoList-services.js";
+
 const app = express();
 const port = 8000;
+
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+// get tasks
+app.get('/tasks', async (req, res) => {
+    const task = req.query.task;
+    const category = req.query.category;
+    const dueDate = req.query.due;
+    const priority = req.query.priority;
+    try {
+        const result = await toDoListServices.getTasks(task, category, dueDate, priority);
+        res.send({tasks_list: result});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("An error occurred in the server.");
+    }
+});
+ 
+// get task by id
+app.get('/tasks/:id', async (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    const result = await toDoListServices.findTaskById(id);
+    if (result === undefined || result === null)
+        res.status(404).send("Resource not found.");
+    else {
+        res.send({taks_list: result});
+    }
+});
+
+// add task
+app.post('/tasks', async (req, res) => {
+    const task = req.body;
+    const newTask = await toDoListServices.addTask(task);
+   
+    if (newTask) res.status(201).json(newTask);
+    else res.status(500).end()
+});
+
+// Delete a task by ID
+app.delete('/tasks/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+      const deletedTask = await toDoListServices.deleteTask(id);
+      if (!deletedTask) {
+        res.status(404).send("Task not found.");
+      } else {
+        res.send({ message: "Task deleted successfully.", deletedTask });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred in the server.");
+    }
+  });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});
-
-const users = { 
-    users_list : [
-       { 
-          id : 'xyz789',
-          name : 'Charlie',
-          job: 'Janitor',
-       },
-       {
-          id : 'abc123', 
-          name: 'Mac',
-          job: 'Bouncer',
-       },
-       {
-          id : 'ppp222', 
-          name: 'Mac',
-          job: 'Professor',
-       }, 
-       {
-          id: 'yay999', 
-          name: 'Tu',
-          job: 'Aspring actress',
-       },
-       {
-          id: 'zap555', 
-          name: 'Dennis',
-          job: 'Bartender',
-       }
-    ]
- }
-
-const findUserByName = (name) => { 
-    return users['users_list']
-        .filter( (user) => user['name'] === name); 
-}
-
-const findUserById = (id) =>
-    users['users_list']
-        .find( (user) => user['id'] === id);
-    
-app.get('/users/:id', (req, res) => {
-    const id = req.params['id']; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined) {
-        res.status(404).send('Resource not found.');
-    } else {
-        res.send(result);
-    }
-});
-
-const addUser = (user) => {
-    users['users_list'].push(user);
-    return user;
-}
-
-const newUser = 
-{
-    "id": "qwe123",
-    "job": "Zookeeper",
-    "name": "Cindy"
- };
-
- const generateRandomID = () => {
-    const randNum = Math.random().toString(36).substring(2, 10);
-    return randNum;
-}
-app.post('/users', (req, res) => {
-    const userToAdd = req.body;
-    const newUserID = generateRandomID();
-    userToAdd.id = newUserID;
-    const newUser = addUser(userToAdd);
-    if (newUser) {
-        res.status(201).json(newUser);   // is this how you return updated representation of the object?
-                                        // how to test return newly created object from POST?
-    }
-    else {
-        res.status(500).send('User could not be added.');
-    }
-});
-
-fetch("http://localhost:8000/users", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newUser),
-});
-
-const delUser = (id) => {
-    let result = findUserById(id);
-    if (result) {
-        const num = users['users_list'].indexOf(result);
-        users['users_list'].splice(num, 1);
-    }
-    return result;
-}
-
-app.delete('/users/:id', (req, res) => {
-    const id = req.params['id']; //or req.params.id
-    let result = delUser(id);
-    if (result === undefined) {
-        res.status(404).send('Resource not found.');
-    } else {
-        res.status(204).send(result);
-    }
-});
-
-const findUserByNameJob = (name, job) => { 
-    return (users['users_list']
-        .filter( (user) => user['name'] === name))
-        .filter( (user) => user['job'] === job); 
-}
-
-app.get('/users', (req, res) => {
-    const name = req.query.name;
-    const job = req.query.job;
-    let result;
-    if (name != undefined && job != undefined){
-        result = findUserByNameJob(name, job);
-        result = {users_list: result};
-        res.send(result);
-    }
-    else if (name != undefined) {
-        result = findUserByName(name);
-        result = {users_list: result};
-        res.send(result);
-    }
-    else{
-        res.send(users);
-    }
 });
