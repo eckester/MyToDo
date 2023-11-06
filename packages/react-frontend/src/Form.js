dc0d66404c4b1a63c9bde87e48fb5a27655cc276
Xia Webster <xmw2003@gmail.com>
2023-11-05 16:30:01 -0800

    Add task buttom

--- packages/react-frontend/src/Form.js
+++ packages/react-frontend/src/Form.js
@@ -11,9 +11,10 @@ function Form(props){
         }
     );
 
+    const [showPopup, setShowPopup] = useState(false)
+
     const categoryOptions = ['School', 'Work', 'Other'];  
     const priorityOptions = ['Low', 'Medium', 'High', 'None'];
-    const statusOptions = ['Not started', 'In progress', 'Completed']
  
     function handleChange(event){
         const{name, value} = event.target;
@@ -44,59 +45,69 @@ function Form(props){
     function submitForm(){
         props.handleSubmit(tasks);
         setTask({task: '', category: tasks['category'], priority: tasks['priority'], due: '', status: '', _id: ''});
+        setShowPopup(false); // close the popup after submitting
     }
 
     return (
-        <form>
-            <label htmlFor="name">Name</label>
-            <input
-                type="text"
-                name="name"
-                id="name"
-                value={tasks.task}
-                onChange={handleChange} />
-            <label htmlFor="category">Category</label>
-            <select
-                name="category"
-                id="category"
-                value={tasks.category}
-                onChange={handleChange}
-            >
-                <option value="">---Choose Category---</option>
-                {categoryOptions.map((option) => (
-                    <option key={option} value={option}>
-                        {option}
-                    </option>
-                ))}
-            </select>
+        <div>
+            <button onClick={() => setShowPopup(true)}>Add Task</button> {/* Add Task button */}
+            {showPopup && (
+                <div className = "popup">
+                    <form>
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            value={tasks.task}
+                            onChange={handleChange} />
+                        <label htmlFor="category">Category</label>
+                        <select
+                            name="category"
+                            id="category"
+                            value={tasks.category}
+                            onChange={handleChange}
+                        >
+                            <option value="">---Choose Category---</option>
+                            {categoryOptions.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
 
-            <label htmlFor="priority">Priority</label>
-            <select
-                name="priority"
-                id="priority"
-                value={tasks.priority}
-                onChange={handleChange}
-            >
-                <option value="">---Choose Priority---</option>
-                {priorityOptions.map((option) => (
-                    <option key={option} value={option}>
-                        {option}
-                    </option>
-                ))}
-            </select>
-            
-            <label htmlFor="date">Due Date</label>
-            <input
-                type="date"
-                name="date"
-                id="date"
-                value={tasks.due}
-                onChange={handleChange} />
-            <input 
-                type="button" 
-                value="Submit" 
-                onClick= {submitForm} />
-        </form>
+                        <label htmlFor="priority">Priority</label>
+                        <select
+                            name="priority"
+                            id="priority"
+                            value={tasks.priority}
+                            onChange={handleChange}
+                        >
+                            <option value="">---Choose Priority---</option>
+                            {priorityOptions.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                        
+                        <label htmlFor="date">Due Date</label>
+                        <input
+                            type="date"
+                            name="date"
+                            id="date"
+                            value={tasks.due}
+                            onChange={handleChange} />
+                        <button type="button" onClick={submitForm}>
+                            Submit
+                        </button>
+                        <button type="button" onClick={() => setShowPopup(false)}>
+                            Cancel
+                        </button>
+                    </form>
+                </div>
+            )}
+        </div>
     );
 }
 
