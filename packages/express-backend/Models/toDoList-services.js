e2bc7ff0648dba837f856f2cefa4489724a4a810
bellaboulais <bjboulai@calpoly.edu>
2023-11-07 11:29:39 -0800

    setting up mongo cloud

--- packages/express-backend/Models/toDoList-services.js
+++ packages/express-backend/Models/toDoList-services.js
@@ -2,10 +2,16 @@
 import mongoose from "mongoose";
 import toDoModel from "./toDoList.js";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 mongoose.set("debug", true);
 
+console.log(">>mongo cluster: " + process.env.MONGODB_URI);
+
 mongoose
-    .connect("mongodb://localhost:27017/myToDo", {
+    .connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
