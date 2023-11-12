a6c71fe5cb349b3cdf71e7352340a70efc512527
Camie <camie@MacBook-Pro-648.local>
2023-11-12 13:51:42 -0800

    Quick fix to last push

--- packages/react-frontend/src/Form.js
+++ packages/react-frontend/src/Form.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./Form.css";
 function Form(props) {
   //let date = Date.now();
   //let str = date.toDateString();
@@ -113,7 +112,7 @@ function Form(props) {
               <option value="">---Choose Priority---</option>
               {priorityOptions.map((option) => (
                 <option key={option} value={option}>
-                  <div className={option}>!</div>
+                  {option}
                 </option>
               ))}
             </select>
