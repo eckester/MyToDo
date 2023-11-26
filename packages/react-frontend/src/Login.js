fdaad04f8c7f1a30df8c7bce350235682b6ecd74
Emma Kester <catherine.emmak@gmail.com>
2023-11-26 14:33:39 -0700

    Added UI for creating account

--- packages/react-frontend/src/Login.js
+++ packages/react-frontend/src/Login.js
@@ -1,25 +1,88 @@
-import React from "react";
 import "./Login.css";
 import Button from "@mui/material/Button";
-
-//const grey = "#C0C2C4";
+import React, { useState } from "react";
 
 function LoginPage() {
+  //const navigate = useNavigate();
+  const [showPopup, setShowPopup] = useState(false);
   return (
     <div>
       <h1 className="centered-content">Login</h1>
       <span className="box">
         <h2 className="center-content">Username</h2>
         <br />
-        <input className="textBox" type="text" />
+        <input
+          style={{ backgroundColor: "#C0C2C4" }}
+          className="textBox"
+          type="text"
+        />
         <br />
         <h2 className="center-content">Password</h2>
         <br />
-        <input className="textBox" type="text" />
+        <input
+          style={{ backgroundColor: "#C0C2C4" }}
+          className="textBox"
+          type="text"
+        />
         <br />
-        <Button bsClass="button" variant="contained">
+        <Button
+          bsClass="button"
+          variant="contained"
+          onClick={() => {
+            alert("this is where the redirect goes");
+          }}
+        >
           Log in
         </Button>
+        <br />
+        <p
+          className={"centered-content"}
+          onClick={() => setShowPopup(true)}
+        >
+          No account? Create one&nbsp;<b>here</b>
+        </p>
+        {showPopup && (
+          <div
+            className="sign-in"
+            style={{ backgroundColor: "#ccd5e8" }}
+          >
+            <form>
+              <label htmlFor="username">
+                Enter your Username:
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                style={{ backgroundColor: "#FFFFFF" }}
+              />
+              <label htmlFor="username">
+                Enter your Password:
+              </label>
+              <input
+                type="text"
+                name="password"
+                id="password"
+                style={{ backgroundColor: "#FFFFFF" }}
+              />
+              <Button
+                className="button"
+                variant="outlined"
+                type="button"
+              >
+                Submit
+              </Button>
+              <Button
+                className="button"
+                variant="outlined"
+                type="button"
+                onClick={() => setShowPopup(false)}
+              >
+                Cancel
+              </Button>
+            </form>
+          </div>
+        )}
       </span>
     </div>
   );
