fbfa0ec8b6d28ae2a1d4fd08ef8937c4a46e7257
Emma Kester <catherine.emmak@gmail.com>
2023-11-26 11:03:52 -0700

    Finished basic UI for login page

--- packages/react-frontend/src/Login.js
+++ packages/react-frontend/src/Login.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./Login.css";
+import Button from "@mui/material/Button";
+
+//const grey = "#C0C2C4";
 
 function LoginPage() {
   return (
     <div>
-      <h1>This is the Login Page</h1>
+      <h1 className="centered-content">Login</h1>
+      <span className="box">
+        <h2 className="center-content">Username</h2>
+        <br />
+        <input className="textBox" type="text" />
+        <br />
+        <h2 className="center-content">Password</h2>
+        <br />
+        <input className="textBox" type="text" />
+        <br />
+        <Button bsClass="button" variant="contained">
+          Log in
+        </Button>
+      </span>
     </div>
   );
 }
