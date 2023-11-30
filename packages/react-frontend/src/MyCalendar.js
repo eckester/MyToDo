c50840a2263a31f063629417b904b9af80696c31
bellaboulais <bjboulai@calpoly.edu>
2023-11-29 21:58:33 -0800

    Calendar set up

--- packages/react-frontend/src/MyCalendar.js
+++ packages/react-frontend/src/MyCalendar.js
@@ -1,5 +1,5 @@
 // MyCalendar.js
-import React from "react";
+import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -8,7 +8,6 @@ import "./MyCalendar.css";
 const localizer = momentLocalizer(moment);
 
 const formatDate = (tasks) => {
-  console.log(tasks[0].due);
   return tasks.map((task) => ({
     ...task,
     due: moment.utc(task.due).format("MM/DD/YY")
@@ -16,23 +15,75 @@ const formatDate = (tasks) => {
 };
 
 const MyCalendar = ({ tasks }) => {
-  console.log(tasks);
-
   const formattedTasks = formatDate(tasks);
 
-  const views = {
-    month: true // Display only the month view
+  const [selectedEvent, setSelectedEvent] = useState(undefined);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSelectedEvent = (event) => {
+    setSelectedEvent(event);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedEvent(undefined);
+  };
+
+  const Modal = ({ selectedEvent, closeModal }) => {
+    return (
+      <div className="modal-overlay" onClick={closeModal}>
+        <div className="modal-content">
+          <button
+            className="modal-exit-button"
+            onClick={closeModal}
+          >
+            X
+          </button>
+          <div className="modal-header">
+            <h2 className="modal-title">
+              {selectedEvent.task}
+            </h2>
+          </div>
+          <div className="modal-details">
+            <p> Date: {selectedEvent.due}</p>
+            {selectedEvent.category && (
+              <p className="details-row">
+                Category: {selectedEvent.category}
+                {selectedEvent.class && ( // Check if class exists
+                  <span style={{ float: "right" }}>
+                    Class: {selectedEvent.class}
+                  </span>
+                )}
+              </p>
+            )}
+            <p>Priority: {selectedEvent.priority}</p>
+          </div>
+        </div>
+      </div>
+    );
   };
 
   return (
     <div className="calendar-container">
+      {showModal && (
+        <Modal
+          selectedEvent={selectedEvent}
+          closeModal={closeModal}
+        />
+      )}
       <Calendar
+        popup={true}
+        defaultDate={new Date()}
         localizer={localizer}
         events={formattedTasks}
         startAccessor="due"
         endAccessor="due"
-        views={views}
+        titleAccessor="task"
+        views={["month"]}
         style={{ height: "100vh" }}
+        selected={selectedEvent}
+        onSelectEvent={(e) => handleSelectedEvent(e)}
       />
     </div>
   );
