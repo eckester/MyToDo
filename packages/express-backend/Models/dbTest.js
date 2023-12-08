37d0ab316bece20ecdadbd0f1f5dfc7a7c8234e8
Camie <camie@MacBook-Pro-648.local>
2023-12-08 10:12:23 -0800

    run prettier

--- packages/express-backend/Models/dbTest.js
+++ packages/express-backend/Models/dbTest.js
@@ -1,38 +1,38 @@
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 let mongo = undefined;
 
-async function setUp(){
-    mongo = await MongoMemoryServer.create();
-    const url = mongo.getUri();
+async function setUp() {
+  mongo = await MongoMemoryServer.create();
+  const url = mongo.getUri();
 
-    await mongoose.connect(url, {
-        useNewUrlParser: true,
-    });
-};
+  await mongoose.connect(url, {
+    useNewUrlParser: true
+  });
+}
 
-async function dropDatabase(){
-    if (mongo) {
-        await mongoose.connection.dropDatabase();
-        await mongoose.connection.close();
-        await mongo.stop();
+async function dropDatabase() {
+  if (mongo) {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongo.stop();
+  }
+}
+
+async function dropCollections() {
+  if (mongo) {
+    const collections = mongoose.connection.collections;
+
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany();
     }
-};
-
-async function dropCollections(){
-    if (mongo) {
-        const collections = mongoose.connection.collections;
-
-        for (const key in collections) {
-            const collection = collections[key];
-            await collection.deleteMany();
-        }
-    }
-};
+  }
+}
 
 export default {
-    setUp,
-    dropDatabase,
-    dropCollections,
+  setUp,
+  dropDatabase,
+  dropCollections
 };
